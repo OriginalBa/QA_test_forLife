fc8b19ec2f0514465e76441efc09fb663ee8f300
Rhalimov <rhalimov@yandex.ru>
2026-06-23 18:34:43 +0300

    HomeWork4 - Оптимизация тестов по материалам урока 4. исправлены PageTest и LoginForm

--- PageTestBox.py
+++ PageTestBox.py
@@ -1,241 +1,126 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
 class TestModelForm1:
-    full_name_locator = "userName"
-    email_locator = "userEmail"
-    submit_locator = "submit"
-    current_locator = "currentAddress"
-    permanent_locator = "permanentAddress"
-    result_locator = "output"
+    full_name_locator = (By.ID, "userName")
+    email_locator = (By.ID, "userEmail")
+    submit_locator = (By.ID, "submit")
+    current_locator = (By.ID, "currentAddress")
+    permanent_locator = (By.ID, "permanentAddress")
+    result_locator = (By.ID, "output")
+    email_css_locator = (By.CSS_SELECTOR, "input[type='email']")
 
     def __init__(self):
+        self.driver = None
+        self.wait = None
+
+    def set_up(self):
         self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-
-    def test_set_up(self):
         self.driver.get("https://qa-guru.github.io/one-page-form/text-box.html")
-        time.sleep(3)  # Пауза, чтобы визуально заметить открытие
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(5)
 
-    def test_tear_down(self):
-        # 5. Закрытие браузера в любом случае
+    def tear_down(self):
         self.driver.quit()
 
-    def test_positive_two_fields(self):
+    def input_full_name(self, name):
+        self.driver.find_element(*self.full_name_locator).send_keys(name)
 
-        try:
-            # 2. Открытие страницы
-            self.test_set_up()
+    def input_email(self, email):
+        self.driver.find_element(*self.email_locator).send_keys(email)
 
-            # 3. Поиск элементов и заполнение полей
-            # Находим поле Full Name по его ID и вводим текст
-            full_name_field = self.driver.find_element(By.ID, self.full_name_locator)
-            full_name_field.send_keys("Роман Халимов")
+    def input_current_address(self, address):
+        self.driver.find_element(*self.current_locator).send_keys(address)
 
-            # Находим поле Email по его ID и вводим текст
-            email_field = self.driver.find_element(By.ID, self.email_locator)
-            email_field.send_keys("ian@neexample.com")
+    def input_permanent_address(self, p_address):
+        self.driver.find_element(*self.permanent_locator).send_keys(p_address)
 
-            # Находим кнопку Submit по ее ID и кликаем
-            submit_button = self.driver.find_element(By.ID, self.submit_locator)
-            submit_button.click()
+    def click_submit(self):
+        submit_btn = self.driver.find_element(*self.submit_locator)
+        self.driver.execute_script("arguments[0].click();", submit_btn)
+        submit_btn.click()
 
-            # 4. Проверка результата
-            time.sleep(3)  # Пауза, чтобы увидеть результат отправки
+    def result(self):
+        return self.driver.find_element(*self.result_locator).text
 
-            # Находим блок с отправленными данными
-            result_box = self.driver.find_element(By.ID, self.result_locator)
-
-            # Проверяем, что в блоке результата появился введенный текст
-            assert "Роман Халимов" in result_box.text
-            assert "ian@neexample.com" in result_box.text
-            print("Тест успешно пройден!")
-
-        finally:
-            # 5. Закрытие браузера в любом случае
-            self.test_tear_down()
-
-    def test_positive_all_fields(self):
-
-        try:
-            # 2. Открытие страницы
-            self.test_set_up()
-
-            # 3. Поиск элементов и заполнение полей
-            # Находим поле Full Name по его ID и вводим текст
-            full_name_field = self.driver.find_element(By.ID, self.full_name_locator)
-            full_name_field.send_keys("Роман Халимов")
-
-            # Находим поле Email по его ID и вводим текст
-            email_field = self.driver.find_element(By.ID, self.email_locator)
-            email_field.send_keys("ian@neexample.com")
-
-            # Находим поле Current Address и заполняем его
-            current_address = self.driver.find_element(By.ID, self.current_locator)  # currentAddress
-            current_address.send_keys("Москва, Севильский бульвар 1, кв 76")
-
-            # Находим поле Permanent Address и также заполняем его
-            permanent_address = self.driver.find_element(By.ID, self.permanent_locator)  # permanentAddress
-            permanent_address.send_keys("Москва, Несевильский небульвар 1, кв. 76")
-
-            # Находим кнопку Submit по ее ID и кликаем
-            submit_button = self.driver.find_element(By.ID, self.submit_locator)
-            submit_button.click()
-
-            # 4. Проверка результата
-            time.sleep(3)  # Пауза, чтобы увидеть результат отправки
-
-            # Находим блок с отправленными данными
-            result_box = self.driver.find_element(By.ID, self.result_locator)
-
-            # Проверяем, что в блоке результата появился введенный текст
-            assert "Роман Халимов" in result_box.text
-            assert "Москва, Севильский бульвар 1, кв 76" in result_box.text
-            assert "Москва, Несевильский небульвар 1, кв. 76" in result_box.text
-            assert "ian@neexample.com" in result_box.text
-            print("Тест успешно пройден!")
-
-        finally:
-            # 5. Закрытие браузера в любом случае
-            self.test_tear_down()
-
-    def test_negative_mail(self):
-
-        try:
-            # 2. Открытие страницы
-            self.test_set_up()
-
-            # 3. Поиск элементов и заполнение полей
-            # Находим поле Full Name по его ID и вводим текст
-            full_name_field = self.driver.find_element(By.ID, self.full_name_locator)
-            full_name_field.send_keys("Роман Халимов")
-
-            # Находим поле Email по его ID и вводим текст
-            email_field = self.driver.find_element(By.ID, self.email_locator)
-            email_field.send_keys("ian@ne_example.com")
-
-            # Находим поле Current Address и заполняем его
-            current_address = self.driver.find_element(By.ID, self.current_locator)  # currentAddress
-            current_address.send_keys("Москва, Севильский бульвар 1, кв 76")
-
-            # Находим поле Permanent Address и также заполняем его
-            permanent_address = self.driver.find_element(By.ID, self.permanent_locator)  # permanentAddress
-            permanent_address.send_keys("Москва, Несевильский небульвар 1, кв. 76")
-
-            # Находим кнопку Submit по ее ID и кликаем
-            submit_button = self.driver.find_element(By.ID, self.submit_locator)
-            submit_button.click()
-
-            # 4. Проверка результата
-            time.sleep(3)  # Пауза, чтобы увидеть результат отправки
-
-            # Находим блок с отправленными данными
-            result_box = self.driver.find_element(By.ID, self.result_locator)
-
-            # Проверяем, что в блоке результата появился введенный текст
-            email_field = self.driver.find_element(By.CSS_SELECTOR, "input[type='email']")
-            validation_message = self.driver.execute_script("return arguments[0].validationMessage", email_field)
-            assert 'Часть адреса после символа "@" не должна содержать символ "_"' in validation_message
-            assert "Роман Халимов" is not result_box.text
-            assert "Москва, Севильский бульвар 1, кв 76" is not result_box.text
-            print("Негативный тест пройден!")
-
-        finally:
-            # 5. Закрытие браузера в любом случае
-            self.test_tear_down()
-
-    def test_negative_nosimbol(self):
-
-        try:
-            # 2. Открытие страницы
-            self.test_set_up()
-
-            # 3. Поиск элементов и заполнение полей
-            # Находим поле Full Name по его ID и вводим текст
-            full_name_field = self.driver.find_element(By.ID, self.full_name_locator)
-            full_name_field.send_keys("Роман Халимов")
-
-            # Находим поле Email по его ID и вводим текст
-            email_field = self.driver.find_element(By.ID, self.email_locator)
-            email_field.send_keys("ianne_example.com")
-
-            # Находим поле Current Address и заполняем его
-            current_address = self.driver.find_element(By.ID, self.current_locator)  # currentAddress
-            current_address.send_keys("Москва, Севильский бульвар 1, кв 76")
-
-            # Находим поле Permanent Address и также заполняем его
-            permanent_address = self.driver.find_element(By.ID, self.permanent_locator)  # permanentAddress
-            permanent_address.send_keys("Москва, Несевильский небульвар 1, кв. 76")
-
-            # Находим кнопку Submit по ее ID и кликаем
-            submit_button = self.driver.find_element(By.ID, self.submit_locator)
-            submit_button.click()
-
-            # 4. Проверка результата
-            time.sleep(3)  # Пауза, чтобы увидеть результат отправки
-
-            # Находим блок с отправленными данными
-            result_box = self.driver.find_element(By.ID, self.result_locator)
-
-            # Проверяем, что в блоке результата появился введенный текст
-            email_field = self.driver.find_element(By.CSS_SELECTOR, "input[type='email']")
-            validation_message = self.driver.execute_script("return arguments[0].validationMessage", email_field)
-            assert 'Адрес электронной почты должен содержать символ "@". В адресе "ianne_example.com" отсутствует символ "@".' in validation_message
-            assert "Роман Халимов" is not result_box.text
-            assert "Москва, Севильский бульвар 1, кв 76" is not result_box.text
-            assert "ian@neexample.com" is not result_box.text
-            print("Негативный тест пройден!")
-
-        finally:
-            # 5. Закрытие браузера в любом случае
-            self.test_tear_down()
-
-    def test_sql_injection(self):
-
-        try:
-            # 2. Открытие страницы
-            self.test_set_up()
-
-            # 3. Поиск элементов и заполнение полей
-            # Находим поле Full Name по его ID и вводим текст
-            full_name_field = self.driver.find_element(By.ID, self.full_name_locator)
-            full_name_field.send_keys("Иван Иванов")
-
-            # Находим поле Email по его ID и вводим текст
-            email_field = self.driver.find_element(By.ID, self.email_locator)
-            email_field.send_keys("AND (SELECT SUBSTRING(password,1,1) FROM users WHERE id=1) = 'a'")
-
-            # Находим кнопку Submit по ее ID и кликаем
-            submit_button = self.driver.find_element(By.ID, self.submit_locator)
-            submit_button.click()
-
-            # 4. Проверка результата
-            time.sleep(3)  # Пауза, чтобы увидеть результат отправки
-
-            # Находим блок с отправленными данными
-            result_box = self.driver.find_element(By.ID, self.result_locator)
-
-            # Проверяем, что в блоке результата появился введенный текст
-            email_field = self.driver.find_element(By.CSS_SELECTOR, "input[type='email']")
-            validation_message = self.driver.execute_script("return arguments[0].validationMessage", email_field)
-            assert 'Адрес электронной почты должен содержать символ "@". В адресе "AND (SELECT SUBSTRING(password,1,1) FROM users WHERE id=1) = \'a\'" отсутствует символ "@".' in validation_message
-            assert "Иван Иванов" is not result_box.text
-            print("Негативный тест пройден!")
-
-        finally:
-            # 5. Закрытие браузера в любом случае
-            self.test_tear_down()
+    def get_email_validation_message(self):
+        email_field = self.driver.find_element(*self.email_css_locator)
+        validation_message = self.driver.execute_script("return arguments[0].validationMessage;", email_field)
+        return validation_message
 
 
-test_model_form = TestModelForm1()
-test_model_form.test_positive_two_fields()
-test_model_form = TestModelForm1()
-test_model_form.test_positive_all_fields()
-test_model_form = TestModelForm1()
-test_model_form.test_negative_mail()
-test_model_form = TestModelForm1()
-test_model_form.test_negative_nosimbol()
-test_model_form = TestModelForm1()
-test_model_form.test_sql_injection()
+def test_positive_two_fields():
+    page_form = TestModelForm1()
+    page_form.set_up()
+    try:
+        page_form.input_full_name("Роман Халимов")
+        page_form.input_email("ian@neexample.com")
+        page_form.click_submit()
+        result_text = page_form.result()
+        assert "Роман Халимов" in result_text
+        assert "ian@neexample.com" in result_text
+        print("Тест успешно пройден!")
+    finally:
+        page_form.tear_down()
+
+
+def test_positive_all_fields():
+    page_form = TestModelForm1()
+    page_form.set_up()
+    try:
+        page_form.input_full_name("Роман Халимов")
+        page_form.input_email("ian@neexample.com")
+        page_form.input_current_address("Москва, Севильский бульвар 1, кв 76")
+        page_form.input_permanent_address("Москва, Несевильский небульвар 1, кв. 76")
+        page_form.click_submit()
+        result_text = page_form.result()
+        assert "Роман Халимов" in result_text
+        assert "ian@neexample.com" in result_text
+        assert "Москва, Севильский бульвар 1, кв 76" in result_text
+        assert "Москва, Несевильский небульвар 1, кв. 76" in result_text
+        print("Тест успешно пройден!")
+    finally:
+        page_form.tear_down()
+
+
+def test_negative_mail():
+    page_form = TestModelForm1()
+    page_form.set_up()
+    try:
+        page_form.input_full_name("Роман Халимов")
+        page_form.input_email("ian@ne_example.com")
+        page_form.click_submit()
+        error_message = page_form.get_email_validation_message()
+        assert 'Часть адреса после символа "@" не должна содержать символ "_"' in error_message
+        print("Негативный тест успешно пройден!")
+    finally:
+        page_form.tear_down()
+
+
+def test_negative_nosimbol():
+    page_form = TestModelForm1()
+    page_form.set_up()
+    try:
+        page_form.input_full_name("Роман Халимов")
+        page_form.input_email("ianne_example.com")
+        page_form.click_submit()
+        error_message = page_form.get_email_validation_message()
+        assert 'Адрес электронной почты должен содержать символ "@". В адресе "ianne_example.com" отсутствует символ "@".' in error_message
+        print("Негативный тест успешно пройден!")
+    finally:
+        page_form.tear_down()
+
+
+def test_sql_injection():
+    page_form = TestModelForm1()
+    page_form.set_up()
+    try:
+        page_form.input_full_name("Роман Халимов")
+        page_form.input_email("SELECT SUBSTRING(password,1,1")
+        page_form.click_submit()
+        error_message = page_form.get_email_validation_message()
+        assert 'Адрес электронной почты должен содержать символ "@". В адресе "SELECT SUBSTRING(password,1,1" отсутствует символ "@".' in error_message
+        print("Негативный тест успешно пройден!")
+    finally:
+        page_form.tear_down()
