1dc4e0f1bc11f9a5706573ebd65d4fea78d68973
Rhalimov <rhalimov@yandex.ru>
2026-08-10 00:08:24 +0300

    HomeWork8 Переделана страница login_test_suite.py и login_form_po.py, pytest.ini. В page_box подправлен маркер регресса

--- login_form_po.py
+++ login_form_po.py
@@ -1,37 +1,38 @@
-from selenium.webdriver.common.by import By
+from seleniumpagefactory.Pagefactory import PageFactory
 
 
-class LoginFormPO:
-    login_input_locator = (By.ID, "login-input")
-    password_input_locator = (By.ID, "password-input")
-    submit_button_locator = (By.ID, "submit-button")
-    logout_button_locator = (By.ID, "logout-button")
-    welcome_message_locator = (By.ID, "welcome-message")
-    error_locator = (By.ID, "error-message")
+class LoginFormPO(PageFactory):
+    locators = {
+        "login_field": ("ID", "login-input"),
+        "password_field": ("ID", "password-input"),
+        "submit_button": ("ID", "submit-button"),
+        "logout_button": ("ID", "logout-button"),
+        "welcome_message": ("ID", "welcome-message"),
+        "error_message": ("ID", "error-message"),
+    }
 
     def __init__(self, driver):
         self.driver = driver
+        self.url = "https://qa-guru.github.io/one-page-form/login.html"
+        self.timeout = 10
 
-    # Low Level action
+    def open(self):
+        self.driver.get(self.url)
+
     def set_login_field(self, login):
-        self.driver.find_element(*self.login_input_locator).send_keys(login)
+        self.login_field.set_text(login)
 
     def set_password_field(self, password):
-        self.driver.find_element(*self.password_input_locator).send_keys(password)
+        self.password_field.set_text(password)
 
     def click_submit_button(self):
-        self.driver.find_element(*self.submit_button_locator).click()
+        self.submit_button.click_button()
 
     def click_logout_button(self):
-        self.driver.find_element(*self.logout_button_locator).click()
+        self.logout_button.click_button()
 
     def get_welcome_message_text(self):
-        return self.driver.find_element(*self.welcome_message_locator).text
+        return self.welcome_message.get_text()
 
     def get_error_text(self):
-        return self.driver.find_element(*self.error_locator).text
-
-# В одном файле находится один класс. Отдельно взятые функции в проекте не используются
-# шаг 1 - класс разбить на два класса. Сделать еще один класс loginTestSuite - в него пойдут тесты
-# шаг 2 добавить логгироание в тесты
-# щаг 3 перенести сетап м тирдаун
+        return self.error_message.get_text()
